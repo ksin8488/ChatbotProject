f0d74415e2d69ac6ce373ef287f61e28ea5854a6
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-11-14 12:15:15 -0700

    Created and Coded ChatFrame and ChatPanel in the chat.view package.
    This will allow the use of a GUI in the chatbot program.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -5,5 +5,24 @@ import javax.swing.JFrame;
 
 public class ChatFrame extends JFrame
 {
-
+	private ChatController appController;
+	private ChatPanel appPane;
+	
+	public ChatFrame(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	 {
+		this.setContentPane(appPane);
+		this.setTitle("Chatting with the chatbot");
+		this.setResizable(false);;
+		this.setSize(600, 600);
+		this.setVisible(true);
+	 }
 }
---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,41 @@
+package chat.view;
+
+import javax.swing.JPanel;
+import chat.controller.ChatController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class ChatPanel extends JPanel
+{
+	private ChatController appController;
+	private SpringLayout appLayout;
+	
+	public ChatPanel(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+}
