2e4a15290475a259f3c72bd13511f7ee07770390
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-03-01 11:59:52 -0700

    Created getMostCommonWord() and collectTweets() methods.
    getMostCommonWord() will get the most common words from a user by looking at their tweets through the use of the collectTweets method which sifts through and analyzis their tweets.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -34,6 +34,48 @@ public class CTECTwitter
 		this.searchedTweets = new ArrayList<Status>();
 		this.tweetedWords = new ArrayList<String>();
 		this.chatbotTwitter = TwitterFactory.getSingleton();
+		this.totalWordCount = 0;
+	}
+	
+	public String getMostCommonWord(String username)
+	{
+		String mostCommon = "";
+		
+		collectTweets(username);
+		
+		return mostCommon;
+	}
+	
+	private void collectTweets(String username)
+	{
+		searchedTweets.clear();
+		tweetedWords.clear();
+		
+		Paging statusPage = new Paging(1, 100);
+		int page = 1;
+		long lastID = Long.MAX_VALUE;
+		
+		while(page <= 10)
+		{
+			statusPage.setPage(page);
+			try
+			{
+				ResponseList<Status> listedTweets = chatbotTwitter.getUserTimeline(username, statusPage);
+				for(Status current : listedTweets)
+				{
+					if(current.getId() < lastID)
+					{
+						searchedTweets.add(current);
+						lastID = current.getId();
+					}
+				}
+			}
+			catch(TwitterException searchTweetError)
+			{
+				appController.handleErrors(searchTweetError);
+			}
+			page++;
+		}
 	}
 	
 	public void sendTweet(String textToTweet)
